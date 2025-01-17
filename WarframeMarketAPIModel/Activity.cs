67d0e6dc61b9138e9a3ed5ecfafeffd62e14ab71
Rafael <rafa.acuna.96@gmail.com>
2025-01-17 13:06:38 -0600

    Release v1.0.1

--- WarframeMarketAPIModel/Activity.cs
+++
@@ -1,6 +0,0 @@
-public class Activity
-{
-    public string Type { get; set; } = string.Empty;
-    public string Details { get; set; } = string.Empty;
-    public DateTime StartedAt { get; set; }
-}
--- WarframeMarketAPIModel/WarframeMarketResponse.cs
+++
@@ -1,6 +0,0 @@
-public class WarframeMarketResponse
-{
-    public string ApiVersion { get; set; } = string.Empty;
-    public Order[]? Data { get; set; }
-    public object? Error { get; set; }
-}
